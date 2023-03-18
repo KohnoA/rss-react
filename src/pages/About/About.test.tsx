2a043ad4e0c9d57bbc58acb49365288ed8b4c790
KohnoA <san.kohno@yandex.ru>
2023-03-19 00:51:48 +0300

    refactor: tests for About and NotFound components

--- src/pages/About/About.test.tsx
+++ src/pages/About/About.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import About from './About';
 
 describe('testing About component', () => {
-  test('should be displayed', () => {
+  it('should be displayed', () => {
     render(<About />);
 
     expect(screen.getByTestId('page-about')).toBeInTheDocument();
--- src/pages/NotFound/NotFound.test.tsx
+++ src/pages/NotFound/NotFound.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import NotFound from './NotFound';
 
 describe('testing NotFound component', () => {
-  test('should be displayed', () => {
+  it('should be displayed', () => {
     render(<NotFound />);
 
     expect(screen.getByTestId('page-not-found')).toBeInTheDocument();
