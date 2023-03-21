da28a3da34a2fbd3de3d4c5a74fe65ce2821faab
KohnoA <san.kohno@yandex.ru>
2023-03-21 23:01:14 +0300

    refactor: moved to global styles, page heading styles

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -1,12 +1,11 @@
 import { Component } from 'react';
-import styles from './Form.module.scss';
 import CreateCard from 'src/components/CreateCard/CreateCard';
 
 export default class Form extends Component {
   render() {
     return (
       <div className="container page" data-testid="page-form">
-        <h2 className={styles.form__title}>Form</h2>
+        <h2 className="title">Form</h2>
 
         <CreateCard />
       </div>
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import styles from './Main.module.scss';
 import SearchPanel from 'src/components/SearchPanel/SearchPanel';
 import CardList from 'src/components/CardList/CardList';
 
@@ -7,7 +6,7 @@ export default class Main extends Component {
   render() {
     return (
       <div className="container page" data-testid="page-main">
-        <h2 className={styles.main__title}>Main</h2>
+        <h2 className="title">Main</h2>
 
         <SearchPanel />
 
