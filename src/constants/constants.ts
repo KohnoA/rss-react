105083b56b7c8563705b12125a44a69caf22c208
KohnoA <san.kohno@yandex.ru>
2023-03-21 22:36:29 +0300

    refactor: created a separate constant under the localstorage key from the SearchPanel value

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,7 @@
 import { IProduct } from 'src/types/IProduct';
 
 export const FAKESTORE_API = 'https://fakestoreapi.com/products?limit=12';
+export const LOCALSTORAGE_KEY_SEARCH = 'search';
 export const PRODUCTS_DATA: IProduct[] = [
   {
     id: 1,
