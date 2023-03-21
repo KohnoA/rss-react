e638b1bffe899b25bfc673d5120d44cf5912c343
KohnoA <san.kohno@yandex.ru>
2023-03-21 23:10:42 +0300

    refactor: tests for CardList components

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,61 +1,10 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import CardList from './CardList';
-import axios from 'axios';
-import { vi } from 'vitest';
-import { IProduct } from 'src/types/IProduct';
-import { act } from 'react-dom/test-utils';
+import { PRODUCTS_DATA } from 'src/constants/productsData';
 
 describe('testing CardList component', () => {
-  vi.mock('axios');
-  const response: IProduct[] = [
-    {
-      id: 1,
-      image: '../../assets/images/mock.jpg',
-      title: 'Mock Item1',
-      category: 'Mock category1',
-      price: 45.99,
-      description: 'mock desc1',
-      rating: {
-        rate: 5,
-        count: 10,
-      },
-    },
-    {
-      id: 2,
-      image: '../../assets/images/mock.jpg',
-      title: 'Mock Item1',
-      category: 'Mock category1',
-      price: 46.99,
-      description: 'mock desc2',
-      rating: {
-        rate: 6,
-        count: 11,
-      },
-    },
-    {
-      id: 3,
-      image: '../../assets/images/mock.jpg',
-      title: 'Mock Item1',
-      category: 'Mock category1',
-      price: 47.99,
-      description: 'mock desc3',
-      rating: {
-        rate: 7,
-        count: 12,
-      },
-    },
-  ];
-
-  afterAll(() => {
-    vi.clearAllMocks;
-    vi.resetAllMocks;
-  });
-
   it('should be displayed', async () => {
-    act(() => {
-      (axios as jest.Mocked<typeof axios>).get.mockResolvedValue({ data: response });
-      render(<CardList />);
-    });
+    render(<CardList />);
 
     await waitFor(() => {
       expect(screen.getByTestId('card-list')).toBeInTheDocument();
@@ -63,14 +12,9 @@ describe('testing CardList component', () => {
   });
 
   it('should display a list of cards', async () => {
-    act(() => {
-      (axios as jest.Mocked<typeof axios>).get.mockResolvedValue({ data: response });
-      render(<CardList />);
-    });
+    render(<CardList />);
 
-    await waitFor(() => {
-      const cardsData = screen.getAllByTestId('card');
-      expect(cardsData.length).toBe(12);
-    });
+    const cardsData = screen.getAllByTestId('card');
+    expect(cardsData.length).toBe(PRODUCTS_DATA.length);
   });
 });
