7f26d2fb3d67390357e72c838fcc471390e96000
KohnoA <san.kohno@yandex.ru>
2023-03-27 14:27:17 +0300

    refactor: Footer.tsx migrated to functional component

--- src/components/UI/Footer/Footer.tsx
+++ src/components/UI/Footer/Footer.tsx
@@ -1,20 +1,17 @@
-import { Component } from 'react';
 import styles from './Footer.module.scss';
 import { RSS_REACT_COURSE_LINK, GITHUB_KOHNOA } from 'src/constants/constants';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer className={styles.footer}>
-        <div className={`container ${styles.footer__wrapper}`}>
-          <a className={styles.footer__link} href={GITHUB_KOHNOA}>
-            KohnoA
-          </a>
-          <a className={styles.footer__link} href={RSS_REACT_COURSE_LINK}>
-            Rsschool
-          </a>
-        </div>
-      </footer>
-    );
-  }
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={`container ${styles.footer__wrapper}`}>
+        <a className={styles.footer__link} href={GITHUB_KOHNOA}>
+          KohnoA
+        </a>
+        <a className={styles.footer__link} href={RSS_REACT_COURSE_LINK}>
+          Rsschool
+        </a>
+      </div>
+    </footer>
+  );
 }
