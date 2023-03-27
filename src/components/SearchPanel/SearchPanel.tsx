7b60798d5ac4644ab70cec573820632e3c1739ca
KohnoA <san.kohno@yandex.ru>
2023-03-27 15:18:45 +0300

    refactor: SearchPanel.tsx migrated to functional component

--- src/components/SearchPanel/SearchPanel.tsx
+++ src/components/SearchPanel/SearchPanel.tsx
@@ -1,58 +1,45 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './SearchPanel.module.scss';
 import { LOCALSTORAGE_KEY_SEARCH } from 'src/constants/constants';
 
-interface SearchPanelState {
-  value: string;
-}
-
-export default class SearchPanel extends Component<unknown, SearchPanelState> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-  }
-
-  componentDidMount(): void {
+export default function SearchPanel() {
+  const [value, setValue] = useState<string>(() => {
     const savedValue = localStorage.getItem(LOCALSTORAGE_KEY_SEARCH);
-    if (savedValue) this.setState({ value: savedValue });
-  }
+    return savedValue ? savedValue : '';
+  });
 
-  componentWillUnmount(): void {
-    localStorage.setItem(LOCALSTORAGE_KEY_SEARCH, this.state.value);
-  }
+  useEffect(() => {
+    return () => localStorage.setItem(LOCALSTORAGE_KEY_SEARCH, value);
+  });
 
-  render() {
-    return (
-      <form
-        className={styles.search}
-        action="#"
-        method="GET"
-        onSubmit={(event) => event.preventDefault()}
-        data-testid="search-panel"
-      >
-        <input
-          className={styles.search__input}
-          type="text"
-          placeholder="I want to find..."
-          value={this.state.value}
-          onChange={(event) => this.setState({ value: event.target.value })}
-        />
+  return (
+    <form
+      className={styles.search}
+      action="#"
+      method="GET"
+      onSubmit={(event) => event.preventDefault()}
+      data-testid="search-panel"
+    >
+      <input
+        className={styles.search__input}
+        type="text"
+        placeholder="I want to find..."
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
 
-        {this.state.value && (
-          <>
-            <span
-              className={styles.search__clean}
-              onClick={() => this.setState({ value: '' })}
-              data-testid="clean-button"
-            />
-            <button className={styles.search__find} type="submit">
-              Search
-            </button>
-          </>
-        )}
-      </form>
-    );
-  }
+      {value && (
+        <>
+          <span
+            className={styles.search__clean}
+            onClick={() => setValue('')}
+            data-testid="clean-button"
+          />
+          <button className={styles.search__find} type="submit">
+            Search
+          </button>
+        </>
+      )}
+    </form>
+  );
 }
