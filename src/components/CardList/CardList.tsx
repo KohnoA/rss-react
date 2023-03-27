68bc9d7e66e8c70dce741194be970e59b793c177
KohnoA <san.kohno@yandex.ru>
2023-03-27 14:42:07 +0300

    refactor: CardList.tsx migrated to functional component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import styles from './CardList.module.scss';
 import Card from '../Card/Card';
 import { IProduct } from 'src/types/IProduct';
@@ -7,18 +6,12 @@ interface CardListProps {
   cardsData: IProduct[];
 }
 
-export default class CardList extends Component<CardListProps> {
-  constructor(props: CardListProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={styles.cardList__list} data-testid="card-list">
-        {this.props.cardsData.map((item) => (
-          <Card key={item.id} data={item} />
-        ))}
-      </div>
-    );
-  }
+export default function CardList({ cardsData }: CardListProps) {
+  return (
+    <div className={styles.cardList__list} data-testid="card-list">
+      {cardsData.map((item) => (
+        <Card key={item.id} data={item} />
+      ))}
+    </div>
+  );
 }
