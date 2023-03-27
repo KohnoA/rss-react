ce85cecce3b00e93f5ef0a7b49a216cec62e9b37
KohnoA <san.kohno@yandex.ru>
2023-03-27 14:38:51 +0300

    refactor: Card.tsx migrated to functional component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { IProduct } from 'src/types/IProduct';
 import Button from '../UI/Button/Button';
 import styles from './Card.module.scss';
@@ -7,53 +6,47 @@ interface CardProps {
   data: IProduct;
 }
 
-export default class Card extends Component<CardProps> {
-  constructor(props: CardProps) {
-    super(props);
-  }
+export default function Card({ data }: CardProps) {
+  const { image, title, category, price, rate, date, condition, tags } = data;
 
-  render() {
-    const { image, title, category, price, rate, date, condition, tags } = this.props.data;
-
-    return (
-      <div className={styles.card} data-testid="card">
-        {tags.bargain && (
-          <div
-            className={`${styles.card__tags} ${styles.card__tags_bargain}`}
-            data-testid="card-bargain"
-          >
-            Bargain
-          </div>
-        )}
-        {tags.urgently && (
-          <div
-            className={`${styles.card__tags} ${styles.card__tags_urgently}`}
-            data-testid="card-urgently"
-          >
-            Urgently
-          </div>
-        )}
-
-        <span
-          className={styles.card__image}
-          style={{ backgroundImage: `url(${image})` }}
-          data-testid="card-image"
-        />
-
-        <p className={styles.card__title}>
-          {title} ({condition})
-        </p>
-
-        <p className={styles.card__category}>{category}</p>
-        <p className={styles.card__price}>&#8364;{price}</p>
-
-        <div className={styles.card__rating}>
-          <span data-testid="card-rate">Rate: {rate}</span>
-          <span data-testid="card-date">Date: {date}</span>
+  return (
+    <div className={styles.card} data-testid="card">
+      {tags.bargain && (
+        <div
+          className={`${styles.card__tags} ${styles.card__tags_bargain}`}
+          data-testid="card-bargain"
+        >
+          Bargain
         </div>
+      )}
+      {tags.urgently && (
+        <div
+          className={`${styles.card__tags} ${styles.card__tags_urgently}`}
+          data-testid="card-urgently"
+        >
+          Urgently
+        </div>
+      )}
 
-        <Button text="Buy Now" additionalClasses={styles.card__button} />
+      <span
+        className={styles.card__image}
+        style={{ backgroundImage: `url(${image})` }}
+        data-testid="card-image"
+      />
+
+      <p className={styles.card__title}>
+        {title} ({condition})
+      </p>
+
+      <p className={styles.card__category}>{category}</p>
+      <p className={styles.card__price}>&#8364;{price}</p>
+
+      <div className={styles.card__rating}>
+        <span data-testid="card-rate">Rate: {rate}</span>
+        <span data-testid="card-date">Date: {date}</span>
       </div>
-    );
-  }
+
+      <Button text="Buy Now" additionalClasses={styles.card__button} />
+    </div>
+  );
 }
