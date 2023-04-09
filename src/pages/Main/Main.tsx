fafa69180800bb3bbcc5df201b5044807cfb0cd9
KohnoA <san.kohno@yandex.ru>
2023-04-10 02:24:05 +0300

    refactor: implementation of a data request in Main component

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { useFetching } from 'src/hooks/useFetching';
 import SearchPanel from 'src/components/SearchPanel/SearchPanel';
 import CardList from 'src/components/CardList/CardList';
 import Loader from 'src/components/UI/Loader/Loader';
@@ -10,16 +9,18 @@ import { LOCALSTORAGE_SEARCH, URL_KEY_SEARCH } from 'src/constants/constants';
 
 export default function Main() {
   const [cardsData, setCardsData] = useState<IProduct[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [searchParams] = useSearchParams();
   const filter =
     searchParams.get(URL_KEY_SEARCH) || localStorage.getItem(LOCALSTORAGE_SEARCH) || '';
-  const [getCardsData, isLoading] = useFetching(async () => {
-    setCardsData(await ProductService.getAll(filter));
-  });
 
   useEffect(() => {
-    getCardsData();
-  }, [filter]); // eslint-disable-line react-hooks/exhaustive-deps
+    setIsLoading(true);
+    ProductService.getAll(filter)
+      .then((res) => setCardsData(res))
+      .catch((err) => console.error(err.message))
+      .finally(() => setIsLoading(false));
+  }, [filter]);
 
   return (
     <div className="container page" data-testid="page-main">
