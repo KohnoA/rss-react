b13dc75aa786c97fa3275998808fe4bbd0c52780
KohnoA <san.kohno@yandex.ru>
2023-04-11 02:36:26 +0300

    feat: creating a modal window through the portal

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { createPortal } from 'react-dom';
 import styles from './Modal.module.scss';
 
 interface ModalProps {
@@ -7,12 +8,13 @@ interface ModalProps {
 }
 
 export default function Modal({ isActive, onClose, children }: ModalProps) {
-  return (
+  return createPortal(
     <div className={`${styles.modal} ${isActive ? styles.modal_active : ''}`} onClick={onClose}>
       <div className={styles.modal__content} onClick={(event) => event.stopPropagation()}>
         <span className={styles.modal__close} onClick={onClose} />
-        {children || <div className={styles.modal__empty}></div>}
+        {children}
       </div>
-    </div>
+    </div>,
+    document.getElementById('modal') as HTMLElement
   );
 }
