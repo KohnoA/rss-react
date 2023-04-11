d1412d40a9069d4cd5f13e412004e2fffac32813
KohnoA <san.kohno@yandex.ru>
2023-04-11 03:48:42 +0300

    refactor: removed the implementation of opening a modal window through the portal

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,4 +1,3 @@
-import { createPortal } from 'react-dom';
 import styles from './Modal.module.scss';
 
 interface ModalProps {
@@ -8,13 +7,16 @@ interface ModalProps {
 }
 
 export default function Modal({ isActive, onClose, children }: ModalProps) {
-  return createPortal(
-    <div className={`${styles.modal} ${isActive ? styles.modal_active : ''}`} onClick={onClose}>
-      <div className={styles.modal__content} onClick={(event) => event.stopPropagation()}>
-        <span className={styles.modal__close} onClick={onClose} />
-        {children}
-      </div>
-    </div>,
-    document.getElementById('modal') as HTMLElement
+  return (
+    <>
+      {isActive && (
+        <div className={styles.modal} onClick={onClose}>
+          <div className={styles.modal__content} onClick={(event) => event.stopPropagation()}>
+            <span className={styles.modal__close} onClick={onClose} />
+            {children}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
