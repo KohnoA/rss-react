840983d8c16d982109287b1bd75f7da106fc0cd8
KohnoA <san.kohno@yandex.ru>
2023-04-23 21:19:58 +0300

    refactor: Footer component, removed tag a

--- src/components/UI/Footer/Footer.tsx
+++ src/components/UI/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 import { RSS_REACT_COURSE_LINK, GITHUB_KOHNOA } from 'src/constants/constants';
 
@@ -5,12 +6,12 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={`container ${styles.footer__wrapper}`}>
-        <a className={styles.footer__link} href={GITHUB_KOHNOA}>
+        <Link className={styles.footer__link} to={GITHUB_KOHNOA}>
           KohnoA
-        </a>
-        <a className={styles.footer__link} href={RSS_REACT_COURSE_LINK}>
+        </Link>
+        <Link className={styles.footer__link} to={RSS_REACT_COURSE_LINK}>
           Rsschool
-        </a>
+        </Link>
       </div>
     </footer>
   );
